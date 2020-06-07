58e3f994a2a5535afa20fb45cade09820ae03d52
zhl <ahfuzl@126.com>
2020-06-07 21:47:29 +0800

    go学习者

--- test/channel.go
+++ test/channel.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
+var a []int		// slice
+var b chan int	// 需要指定通道中元素的类型
 func main() {
-	
+	fmt.Println(b)		// nil
+	b = make(chan int)	// 通道的初始化，通道必须使用make函数初始化才能使用！
+	fmt.Println(b)
 }
