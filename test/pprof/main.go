8019ce39996f0fa8a99b399bbf91a64e3e262022
zhl <zhanglin@vdongchina.com>
2020-07-10 21:03:25 +0800

    go学习者

--- test/pprof/main.go
+++ test/pprof/main.go
@@ -13,42 +13,46 @@ func logicCode() {
 	var c chan int
 	for {
 		select {
-		case v := <-c:
+		case v := <-c:		// 阻塞
 			fmt.Printf("recv from chan, value:%v\n", v)
 		default:
-
+			time.Sleep(time.Millisecond * 500)		//新增的一行内容
 		}
 	}
 }
 
 func main() {
-	var isCPUPprof bool
-	var isMemPprof bool
+	var isCPUPprof bool		// 是否开启CPU profile的标志位
+	var isMemPprof bool		// 是否开启内容profile的标志位
 
 	flag.BoolVar(&isCPUPprof, "cpu", false, "turn cpu pprof on")
 	flag.BoolVar(&isMemPprof, "mem", false, "turn mem pprof on")
 	flag.Parse()
 
 	if isCPUPprof {
-		file, err := os.Create("./cpu.pprof")
+		file1, err := os.Create("./cpu.pprof")		// 在当前路径下创建一个cpu.pprof文件
 		if err != nil {
 			fmt.Printf("create cpu pprof failed, err:%v\n", err)
 			return
 		}
-		pprof.StartCPUProfile(file)
-		defer pprof.StopCPUProfile()
+		pprof.StartCPUProfile(file1)		// 往文件中记录CPU profile信息
+		defer func() {
+			pprof.StopCPUProfile()
+			file1.Close()
+		}()
 	}
 	for i := 0; i < 4; i++ {
 		go logicCode()
 	}
 	time.Sleep(20 * time.Second)
+
 	if isMemPprof {
-		file, err := os.Create("./mem.pprof")
+		file2, err := os.Create("./mem.pprof")
 		if err != nil {
 			fmt.Printf("create mem pprof failed, err:%v\n", err)
 			return
 		}
-		pprof.WriteHeapProfile(file)
-		file.Close()
+		pprof.WriteHeapProfile(file2)
+		file2.Close()
 	}
 }
