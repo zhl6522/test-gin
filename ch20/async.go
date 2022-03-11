6ba0af7c0552f958879fca21f9d12e31db129088
zhl <zhanglin@vdongchina.com>
2022-03-11 17:40:29 +0800

    async

--- ch20/async.go
+++ ch20/async.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"sync"
 	"time"
 )
 
 func main() {
+	rwMutex()
+	
 	r := gin.Default()
 	// 异步
 	r.GET("long_async", func(c *gin.Context) {
@@ -24,4 +28,28 @@ func main() {
 		log.Println("同步执行："+c.Request.URL.Path)
 	})
 	r.Run()
+}
+
+func rwMutex() {
+	var rwm sync.RWMutex
+
+	for i := 0; i <= 2; i++ {
+		go func(i int) {
+			fmt.Printf("go(%d) start lock\n", i)
+			rwm.RLock()
+			fmt.Printf("go(%d) locked\n", i)
+			time.Sleep(time.Second * 2)
+			rwm.RUnlock()
+			fmt.Printf("go(%d) unlock\n", i)
+		}(i)
+	}
+	// 先sleep一小会，保证for的goroutine都会执行
+	time.Sleep(time.Microsecond * 100)
+	fmt.Println("main start lock")
+	// 当子进程都执行时，且子进程所有的资源都已经Unlock了
+	// 父进程才会执行
+	rwm.Lock()
+	fmt.Println("main locked")
+	time.Sleep(time.Second)
+	rwm.Unlock()
 }
